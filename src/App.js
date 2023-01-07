6ff3cf7c5c82be98c5ebf1b83641c83f805709be
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-07 22:37:53 +0200

    Add file Weather.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project created by Olha Kolisnyk{" "}
-          <a href="https://github.com/Olha1304/final-homework-5.git">
+          <a
+            href="https://github.com/Olha1304/final-homework-5.git"
+            target="_blank"
+          >
             open-sourced in GitHub{" "}
           </a>
           and hosted by Netlify
