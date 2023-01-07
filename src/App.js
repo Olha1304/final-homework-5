741f749547e8435fc2cf7e0fb319de029bac3388
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-07 22:22:55 +0200

    Want to check bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project created by Olha Kolisnyk open-sourced in GitHub and hosted
-        by Netlify
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project created by Olha Kolisnyk open-sourced in GitHub and
+          hosted by Netlify
+        </footer>
+      </div>
     </div>
   );
 }
