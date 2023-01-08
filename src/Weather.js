0bdff049aae5bb0952adbafa4896c84bf9430e75
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-08 15:37:49 +0200

    axios in live

--- src/Weather.js
+++ src/Weather.js
@@ -1,43 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
+
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>New York, USA</h1>
+        <ul>
+          <li>Saturday 11:05</li>
+          <li>Cloudly</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
+          <div className="col-6">
+            <img
+              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+              alt="Clear"
+              id="icon"
             />
+            <div className="float-left">
+              <span className="temperature">{temperature}</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Humidity: 72%%</li>
+              <li>Precipitation: 15%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New York, USA</h1>
-      <ul>
-        <li>Saturday 11:05</li>
-        <li>Cloudly</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
-            alt="Clear"
-            id="icon"
-          />
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humadity</li>
-            <li>Participitatiom</li>
-            <li>Speed of wind</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8bec159a2132e758bdo01843fdtc7d40";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
