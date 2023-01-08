5caf02bcbac682bfdd67c07fd94f31adfb267f0d
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-08 13:51:37 +0200

    Add css

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>New York, USA</h1>
       <ul>
         <li>Saturday 11:05</li>
@@ -10,7 +24,11 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img src="image.png" />
+          <img
+            src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+            alt="Clear"
+            id="icon"
+          />
         </div>
         <div className="col-6">
           <ul>
